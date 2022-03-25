d3893463d0cf05378a06957261a4bbca19e46205
rahul <rahul@gamil.com>
2022-03-25 18:31:57 +0530

    this is first commit of git tutorial

---
+++ git.py
@@ -0,0 +1,74 @@
+'''  this nots for those who dont know abut the basic commaand of cammand promt 
+'''
+'''
+// crtl + c is for exit from latest command 
+// cd..  for one step back from directory for ex E:\vscode\git\tut1 this is current directory after entering commnad cd.. you will be in previous directory for ex E:\vscode\git
+//  dir is used for  displying all directory are in current directory
+// dir for list of directory present 
+// E:\competevscode\webdevlopment\nodejs> is the directory locaton
+// _ id previous varibale ingnore++
+// for runing live server click alt+left key on local host Server running at http://127.0.0.1:3000/
+// md for made dirctory , it will create new folder in current dirctory u can check by using dir 
+// cls for cleareing tab "clearing the previous cammand are showing in screen" 
+// rd for remove directory "rd foldername" it will delete that folder 
+// repl is read evaluate print loop , this is used for runnig any code 
+// for setting path you need to go  file(my computer ) copy the path of node js "C:\Program Files\nodejs" and paste  it in envourmental variable 
+1 copy the path of node js , python , where it is installed "most of the times programs files install in c folder in programs file , check your pc location of programe where it was installed ""C:\Program Files\nodejs"
+2 then right click on this pc => clicl on  properties last option on that column " click on advance systems setting (right side on screen , blue text , 4th option from top )
+3 small pop up window will open  click advance option (defalut) after that click on envourmental varibale in systems varibales click on path click on edit , new pop up window will open click on new , paste the copied path here .after that click on ohk . 
+
+
+
+// click on this pc on files , right click and click on properties , 4th option advance system setting , envourmnetal variable , path , edit , new , copy the "C:\Program Files\nodejs" paste it on that new path , then clicl on ok , ok , ok
+
+// for going direct inside the directory    cd .\users\rahul\downloads
+// dir
+// cd 
+// cd.. 
+// cd\ for going inside the server deep 
+// color y   
+// below color changes color of text and bakcground color in command prommnt and power shell
+
+    // 0 = Black       8 = Gray
+    // 1 = Blue        9 = Light Blue
+    // 2 = Green       A = Light Green
+    // 3 = Aqua        B = Light Aqua
+    // 4 = Red         C = Light Red
+    // 5 = Purple      D = Light Purple
+    // 6 = Yellow      E = Light Yellow
+    // 7 = White       F = Bright White
+
+
+    // below color changes color of background 
+    // 07 = Black       80 = Gray
+    // 10 = Blue        90 = Light Blue
+    // 20 = Green       A0 = Light Green
+    // 30 = Aqua        B0 = Light Aqua
+    // 40 = Red         C0 = Light Red
+    // 50 = Purple      D0 = Light Purple
+    // 60 = Yellow      E0 = Light Yellow
+    // 70 = White       F0= Bright White
+
+    you also can change color of text and backgrond color at a time for ex   color f7  : f will aply in background and 7 will aply for text colore ,  more exmaple like a7 , 6a , color e5 is best color as per my view point 
+
+    // this code change color of text  for ex color 4 it change color if text 
+    // color 40 it changes bakcground color
+// color..
+
+// for getting acces to files which contiains spaces in name 
+
+
+// E:\cad , it languge\CAD SOFTWARES>cd ./fusion 360
+
+// E:\cad , it languge\CAD SOFTWARES\FUSION 360>cd..
+
+// E:\cad , it languge\CAD SOFTWARES>cd..
+
+// E:\cad , it languge>cd ./cad softwares/fusion 360
+
+// E:\cad , it languge\CAD SOFTWARES\FUSION 360>
+
+// sure backward slash and file name 
+// also we can use cd "file name "
+
+    '''
